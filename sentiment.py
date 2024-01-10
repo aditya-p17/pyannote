b8f56cb62843082ecc9936189ee0a3a2b10d00b2
Nmn <nssachdeva2@gmail.com>
2024-01-11 01:28:57 +0530

    Sound to text to sentiment analysis

--- sentiment.py
+++ sentiment.py
@@ -15,36 +15,54 @@ headers = {"Authorization": f"Bearer {API}"}
 
 
 def query_sentiment(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-output=query_sentiment({
-	"inputs": "Damn it's not working!!!"
-})
-	
-
-
-print(output[0][0])
-
-APT_TOKEN="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"
-API_URL = "https://api-inference.huggingface.co/models/Baghdad99/saad-speech-recognition-hausa-audio-to-text"
-headers = {"Authorization": f"Bearer {APT_TOKEN}"}
-
-def query_att(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
+    response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
-output1 = query_att("1.mp3")
-text=str(output1["text"])
-print(text)
+# output=query_sentiment({
+#     "inputs": "Damn it's not working!!!"
+# })
+    
+
+# print(output[0][0])
+
+# APT_TOKEN="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"
+# API_URL = "https://api-inference.huggingface.co/models/Baghdad99/saad-speech-recognition-english-audio-to-text" # Updated API URL
+# headers = {"Authorization": f"Bearer {API}"}
+
+# def query_att(filename):
+#     with open(filename, "rb") as f:
+#         data = f.read()
+#     response = requests.post(API_URL, headers=headers, data=data)
+#     return response.json()
+# output1 = query_att("1.mp3")
+# text=output1
+# print(text)
 # output = query_sentiment()
 # print(output)
 
 
-output=query_sentiment({
-	"inputs": output1
+API_URL_SOUND = "https://api-inference.huggingface.co/models/Baghdad99/saad-speech-recognition-hausa-audio-to-text"
+headers = {"Authorization": f"Bearer {API}"}
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL_SOUND, headers=headers, data=data)
+    return response.json()
+
+output = query("1.mp3")
+text = str(output["text"])
+print(text)
+
+senti = query_sentiment({
+    "inputs": text
 })
-print(output)
+
+print(senti[0][0])
+
+# output=query_sentiment({
+#     "inputs": text
+# })
+# print(output)
 #File upload
 #transcript
 #transcript ka sentiment analysis
