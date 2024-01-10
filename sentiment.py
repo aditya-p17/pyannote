c873476960beb88079669a11561b35b3172cd4fa
aditya617p@gmail.com <youremail@example.com>
2024-01-11 02:31:02 +0530

    removed comments

--- sentiment.py
+++ sentiment.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 import os
 
 API_URL = "https://api-inference.huggingface.co/models/arpanghoshal/EmoRoBERTa"
-#headers = {"Authorization": f"Bearer {API} "}
 
-# Load the .env file
+
 load_dotenv()
 
 # Get the API key from the environment variables
@@ -17,27 +16,10 @@ headers = {"Authorization": f"Bearer {API}"}
 def query_sentiment(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
-# output=query_sentiment({
-#     "inputs": "Damn it's not working!!!"
-# })
+
     
 
-# print(output[0][0])
 
-# APT_TOKEN="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"
-# API_URL = "https://api-inference.huggingface.co/models/Baghdad99/saad-speech-recognition-english-audio-to-text" # Updated API URL
-# headers = {"Authorization": f"Bearer {API}"}
-
-# def query_att(filename):
-#     with open(filename, "rb") as f:
-#         data = f.read()
-#     response = requests.post(API_URL, headers=headers, data=data)
-#     return response.json()
-# output1 = query_att("1.mp3")
-# text=output1
-# print(text)
-# output = query_sentiment()
-# print(output)
 
 
 API_URL_SOUND = "https://api-inference.huggingface.co/models/Baghdad99/saad-speech-recognition-hausa-audio-to-text"
@@ -49,7 +31,7 @@ def query(filename):
     response = requests.post(API_URL_SOUND, headers=headers, data=data)
     return response.json()
 
-output = query("1.mp3")
+output = query("2.mp3")
 text = str(output["text"])
 print(text)
 
@@ -59,10 +41,8 @@ senti = query_sentiment({
 
 print(senti[0][0])
 
-# output=query_sentiment({
-#     "inputs": text
-# })
-# print(output)
+
+
 #File upload
 #transcript
 #transcript ka sentiment analysis
