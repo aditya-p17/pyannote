3d364059e01c6da817c45ace7a50623bac2171d0
aditya617p@gmail.com <youremail@example.com>
2024-01-11 00:59:48 +0530

    added whisper

--- sentiment.py
+++ sentiment.py
@@ -3,16 +3,37 @@ import requests
 API_URL = "https://api-inference.huggingface.co/models/arpanghoshal/EmoRoBERTa"
 headers = {"Authorization": f"Bearer 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"}
 
-def query(payload):
+def query_sentiment(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
-output = query({
-	"inputs": "Frankly, my dear, I don't give a damn.",
+output=query_sentiment({
+	"inputs": "Damn it's not working!!!"
 })
+	
+
 
 print(output[0][0])
 
+APT_TOKEN="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"
+API_URL = "https://api-inference.huggingface.co/models/Baghdad99/saad-speech-recognition-hausa-audio-to-text"
+headers = {"Authorization": f"Bearer {APT_TOKEN}"}
+
+def query_att(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+output1 = query_att("1.mp3")
+text=str(output1["text"])
+print(text)
+# output = query_sentiment()
+# print(output)
+
+
+output=query_sentiment({
+	"inputs": output1
+})
+print(output)
 #File upload
 #transcript
 #transcript ka sentiment analysis
